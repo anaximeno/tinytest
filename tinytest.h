06d7454061075dffe4240440373a7b3e6184f078
anaximeno <anaximenobrito@gmail.com>
2023-07-07 00:54:09 -0100

    refactor: move code implementation to a dot c file

--- tinytest.h
+++ tinytest.h
@@ -74,72 +74,12 @@
 #define TT_COLOR_GREEN "[1;32m"
 #define TT_COLOR_RESET "[0m"
 
-static int tt_passes = 0;
-static int tt_fails = 0;
-static int tt_current_test_failed = 0;
-static char *tt_current_msg = NULL;
-static char *tt_current_expression = NULL;
-static char *tt_current_file = NULL;
-static int tt_current_line = 0;
+void tt_execute(const char *name, void (*test_function)());
 
-void tt_execute(const char *name, void (*test_function)())
-{
-  tt_current_test_failed = 0;
-  test_function();
-  if (tt_current_test_failed)
-  {
-    printf("failure: %s:%d: In test %s():\n    %s (%s)\n",
-           tt_current_file, tt_current_line, name, tt_current_msg, tt_current_expression);
-    tt_fails++;
-  }
-  else
-  {
-    tt_passes++;
-  }
-}
+int tt_assert(const char *file, int line, const char *msg, const char *expression, int pass);
 
-int tt_assert(const char *file, int line, const char *msg, const char *expression, int pass)
-{
-  tt_current_msg = msg;
-  tt_current_expression = expression;
-  tt_current_file = file;
-  tt_current_line = line;
-  tt_current_test_failed = !pass;
-  return pass;
-}
+void tt_reset(void);
 
-int tt_report(void)
-{
-  int outcode = 0;
-
-  if (tt_fails)
-  {
-    printf("%c%sFAILED%c%s [%s] (passed:%d, failed:%d, total:%d)\n",
-           TT_COLOR_CODE, TT_COLOR_RED, TT_COLOR_CODE, TT_COLOR_RESET,
-           tt_current_file, tt_passes, tt_fails, tt_passes + tt_fails);
-    outcode = -1;
-  }
-  else
-  {
-    printf("%c%sPASSED%c%s [%s] (total:%d)\n",
-           TT_COLOR_CODE, TT_COLOR_GREEN, TT_COLOR_CODE, TT_COLOR_RESET,
-           tt_current_file, tt_passes);
-  }
-
-  tt_reset();
-
-  return outcode;
-}
-
-void tt_reset(void)
-{
-  int tt_passes = 0;
-  int tt_fails = 0;
-  int tt_current_test_failed = 0;
-  char *tt_current_msg = NULL;
-  char *tt_current_expression = NULL;
-  char *tt_current_file = NULL;
-  int tt_current_line = 0;
-}
+int tt_report(void);
 
 #endif
